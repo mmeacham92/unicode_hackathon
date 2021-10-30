93036ed33ce70bd9078be9da72a024a0c9794274
mmeacham92 <meacham.michael@rocketmail.com>
2021-10-30 18:22:16 -0500

    added render functions; token list now renders correctly when a token is searched

--- main.js
+++ main.js
@@ -1,42 +1,60 @@
 const input = document.querySelector("input");
 const button = document.querySelector("button");
-const tokenDescription = document.querySelector('#token_description');
-const details = document.querySelector('details');
+const tokenDescription = document.querySelector("#token_description");
+const details = document.querySelector("details");
 
 const searchedTokensList = [];
 let currentToken = {};
 
-
-
-
 function getTokenData(token) {
-  searchedTokensList.push(token);
   fetch(`https://api.coingecko.com/api/v3/coins/${token}
 `)
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
       currentToken = {
-          name: token,
-          symbol: data.symbol,
-          currentPrice: data.market_data.current_price.usd,
-          description: data.description.en
+        name: token,
+        symbol: data.symbol,
+        currentPrice: data.market_data.current_price.usd,
+        description: data.description.en,
+      };
+      if (
+        !searchedTokensList.some(
+          (searchedToken) => searchedToken.name === token
+        )
+      ) {
+        searchedTokensList.push(currentToken);
       }
-      details.classList.toggle('hidden');
-      tokenDescription.innerHTML = currentToken.description;
-        const currentPrice = document.querySelector('#price');
-        const fetchedPrice = Number(currentToken.currentPrice).toFixed(2);
-        currentPrice.innerText = fetchedPrice;
+
+      renderSections();
     });
 }
 
+function renderSections() {
+  renderSearchSection();
+  renderTokenList();
+}
+
+function renderSearchSection() {
+  details.classList.remove("hidden");
+  tokenDescription.innerHTML = currentToken.description;
+  const currentPrice = document.querySelector("#price");
+  const fetchedPrice = Number(currentToken.currentPrice).toFixed(2);
+  currentPrice.innerText = fetchedPrice;
+}
+
 function renderTokenList() {
-    const tokenList = document.querySelector('.tokens_list');
-    searchedTokensList.forEach(searchedToken => {
-        const listItem = document.createElement('li');
-        listItem.textContent = searchedToken;
-        tokenList.append(listItem);
-    });
+  const tokenList = document.querySelector(".tokens_list");
+  tokenList.innerHTML = "";
+  searchedTokensList.forEach((searchedToken) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = `${
+      searchedToken.name
+    } (${searchedToken.symbol.toUpperCase()}) ----- $${
+      searchedToken.currentPrice
+    }`;
+    tokenList.append(listItem);
+  });
 }
 
 let update;
