d008e5ac28f64b85790ecf8b12717d27d77cda16
mmeacham92 <meacham.michael@rocketmail.com>
2021-10-30 17:34:44 -0500

    added a details element to house a searched token's description; also created a currentToken object to be used throughout

--- main.js
+++ main.js
@@ -1,19 +1,42 @@
-let input = document.querySelector('input');
-let button = document.querySelector('button');
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const tokenDescription = document.querySelector('#token_description');
+const details = document.querySelector('details');
+
+const searchedTokensList = [];
+let currentToken = {};
+
+
+
 
 function getTokenData(token) {
+  searchedTokensList.push(token);
   fetch(`https://api.coingecko.com/api/v3/coins/${token}
-`).then(response => response.json())
-  .then(data => {
-  console.log(data);
-//   let currentPrice = document.querySelector('#price');
-//   let fetchedPrice = Number(data.data.priceUsd).toFixed(2);
-//   currentPrice.innerText = fetchedPrice;
-});
+`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      currentToken = {
+          name: token,
+          symbol: data.symbol,
+          currentPrice: data.market_data.current_price.usd,
+          description: data.description.en
+      }
+      details.classList.toggle('hidden');
+      tokenDescription.innerHTML = currentToken.description;
+        const currentPrice = document.querySelector('#price');
+        const fetchedPrice = Number(currentToken.currentPrice).toFixed(2);
+        currentPrice.innerText = fetchedPrice;
+    });
 }
 
-function addTokenToList() {
-
+function renderTokenList() {
+    const tokenList = document.querySelector('.tokens_list');
+    searchedTokensList.forEach(searchedToken => {
+        const listItem = document.createElement('li');
+        listItem.textContent = searchedToken;
+        tokenList.append(listItem);
+    });
 }
 
 let update;
@@ -26,7 +49,7 @@ let update;
 // }, 1000);
 // })
 
-button.addEventListener('click', function() {
-    let token = input.value.toLowerCase();
-    getTokenData(token)
-})
+button.addEventListener("click", function () {
+  let token = input.value.toLowerCase();
+  getTokenData(token);
+});
