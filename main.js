ed57dba65c8a9c6afd9f2f27f8d5af2182c259fd
mmeacham92 <meacham.michael@rocketmail.com>
2021-10-30 16:53:42 -0500

    switched to using the CoinGecko API

--- main.js
+++ main.js
@@ -1,23 +1,32 @@
 let input = document.querySelector('input');
 let button = document.querySelector('button');
 
-function getCoinData(coin) {
-  fetch(`https://api.coincap.io/v2/assets/${coin}
+function getTokenData(token) {
+  fetch(`https://api.coingecko.com/api/v3/coins/${token}
 `).then(response => response.json())
   .then(data => {
   console.log(data);
-  let currentPrice = document.querySelector('#price');
-  let fetchedPrice = Number(data.data.priceUsd).toFixed(2);
-  currentPrice.innerText = fetchedPrice;
+//   let currentPrice = document.querySelector('#price');
+//   let fetchedPrice = Number(data.data.priceUsd).toFixed(2);
+//   currentPrice.innerText = fetchedPrice;
 });
+}
+
+function addTokenToList() {
+
 }
 
 let update;
 
+// button.addEventListener('click', function() {
+//   clearInterval(update);
+//   let token = input.value.toLowerCase();
+//   update = setInterval(() => {
+//   getTokenData(token);
+// }, 1000);
+// })
+
 button.addEventListener('click', function() {
-  clearInterval(update);
-  let coin = input.value.toLowerCase();
-  update = setInterval(() => {
-  getCoinData(coin);
-}, 1000);
+    let token = input.value.toLowerCase();
+    getTokenData(token)
 })
